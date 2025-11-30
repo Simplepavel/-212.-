901413457a9c19764b24b9a857c289297c6e3827
n0elloun <lenok5444@gmail.com>
2025-12-01 02:02:23 +0300

    add rating update method

--- rating system/rating.cpp
+++ rating system/rating.cpp
@@ -1,2 +1,18 @@
 #include "rating.h"
+#include<cmath>
 
+void Rating::updateRating(int opponentRating, double score) {
+    const double K = 32;
+    double exp = 1.0/(1.0 + pow(10.0, (opponentRating - rating) / 400.0));//ожидаемый результат
+    int delta = (int)(K * (score - exp));
+    rating += delta;
+    if (score == 1){
+        stats.recordWin();
+    }
+    else if (score == 0){
+        stats.recordLoss();
+    }
+    else{
+         stats.recordDraw();
+    }
+}
