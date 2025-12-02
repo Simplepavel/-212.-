cc35a03aef13ca91270ea4b74793a90049acde65
Pavlov Arseny <catdog09073@gmail.com>
2025-12-02 19:08:41 +0300

    fix: fix bd path

--- DataBase/database.cpp
+++ DataBase/database.cpp
@@ -1,27 +1,21 @@
 #include "database.hpp"
 
-std::string url_base = "postgresql://postgres:NiPWYEfWWdhjhkATtEeg-g7ZD@localhost:5432/durak";
+std::string url_base = "postgresql://postgres:gfhjkm123@localhost:5432/chess";
 
-pqxx::connection *make_session()
-{
-    pqxx::connection *cx = nullptr;
-    try
-    {
-        cx = new pqxx::connection{url_base}; // все будет хорошо
-    }
-    catch (std::exception &error)
-    {
-        std::cout << error.what();
-        return nullptr;
-    }
-    return cx;
+pqxx::connection* make_session() {
+  pqxx::connection* cx = nullptr;
+  try {
+    cx = new pqxx::connection{url_base};  // все будет хорошо
+  } catch (std::exception& error) {
+    std::cout << error.what();
+    return nullptr;
+  }
+  return cx;
 }
-bool delete_session(pqxx::connection *cx)
-{
-    if (cx != nullptr)
-    {
-        delete cx;
-        return true;
-    }
-    return false;
+bool delete_session(pqxx::connection* cx) {
+  if (cx != nullptr) {
+    delete cx;
+    return true;
+  }
+  return false;
 }
